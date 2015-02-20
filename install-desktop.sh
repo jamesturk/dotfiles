12c369a5e8490adc567429fca177f0b1ea9fb8ba
James Turk <james.p.turk@gmail.com>
2015-02-20 16:26:54 -0500

    more tweaks to install

--- install-desktop.sh
+++ install-desktop.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
 # done manually for now
-#   * install chrome
 #   * keyboard layout > options > key sequence to kill x server
 
 # remove the default Ubuntu cruft
 rmdir Documents/ Music/ Public/ Pictures/ Templates/ Videos/ examples.desktop
 
-
-
 # install some things we're going to want
 sudo apt-get install aptitude
 sudo aptitude install \
-    build-essential python-dev libxslt-dev \
-    python-flake8 ipython ipython3 python-pip python-virtualenv virtualenvwrapper \
-    git mercurial subversion \
-    nautilus-dropbox terminator meld gitg \
+    build-essential libxslt1-dev \
+    python3 python-dev python3-dev pypy pypy-dev ipython ipython3 \
+    python-flake8 python-pip python-virtualenv virtualenvwrapper python-tox \
+    zsh vim-nox exuberant-ctags git tmux \
+    nautilus-dropbox meld gitg \
     ack-grep tree s3cmd mosh \
-    mongodb postgresql sqllite3 \
-    vim-nox exuberant-ctags \
-    gcolor2 inkscape keepassx \
-    awesome awesome-extra
+    gcolor2 libgnome2-bin \
+    awesome awesome-extra \
+    xfonts-terminus fonts-droid
 
 chsh -s /bin/zsh
