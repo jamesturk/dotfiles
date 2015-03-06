f395106de29fe700437218abba162d43b5fb75fb
James Turk <james.p.turk@gmail.com>
2015-03-06 15:46:07 -0500

    moving some of this into cer

--- install-desktop.sh
+++ install-desktop.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
 
-# done manually for now
-#   * keyboard layout > options > key sequence to kill x server
-
-# remove the default Ubuntu cruft
-rmdir Documents/ Music/ Public/ Pictures/ Templates/ Videos/ examples.desktop
-
 # install some things we're going to want
-sudo apt-get install aptitude
-sudo aptitude install \
-    build-essential libxslt1-dev \
-    python3 python-dev python3-dev pypy pypy-dev ipython ipython3 \
-    python-flake8 python-pip python-virtualenv virtualenvwrapper python-tox \
-    zsh vim-nox exuberant-ctags git tmux \
-    nautilus-dropbox meld gitg \
-    ack-grep tree s3cmd mosh \
-    sysdig \
-    gcolor2 libgnome2-bin \
-    awesome awesome-extra \
-    xfonts-terminus fonts-droid
+ack-grep 
+awesome
+awesome-extra 
+build-essential
+exuberant-ctags
+fonts-droid
+gcolor2 
+git
+gitg 
+ipython
+ipython3 
+libgnome2-bin 
+libxslt1-dev 
+meld
+mosh 
+nautilus-dropbox
+pypy
+pypy-dev
+python-dev
+python-flake8
+python-pip
+python-tox
+python-virtualenv
+python3
+python3-dev
+s3cmd
+sysdig 
+tmux 
+tree
+vim-nox
+virtualenvwrapper
+xfonts-terminus
+zsh
 
+sudo pip install glances
 chsh -s /bin/zsh
