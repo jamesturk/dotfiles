567ece55f6c0e9df4477f90fc94e85ebc6678921
James Turk <james.p.turk@gmail.com>
2015-03-17 17:50:31 -0400

    drop _gnome-awesome

--- _gnome-awesome/install.sh
+++
@@ -1,5 +0,0 @@
-cp awesome.session /usr/share/gnome-session/sessions/
-cp awesome.desktop /usr/share/applications/
-cp awesome-gnome.desktop /usr/share/xsessions/
-
-# add Awesome to OnlyShowIn key in all /etc/xdg/autostart/ files
