b3bd389d247a5376e03a218381c9fba5ca0be093
James Turk <James@jpt-macbook.local>
2015-08-04 15:52:02 -0400

    link script

---
+++ link.sh
@@ -0,0 +1,11 @@
+ln -s ./dotfiles/.ackrc         ~/.ackrc
+ln -s ./dotfiles/.aliases       ~/.aliases
+# skip bin, config, fonts
+ln -s ./dotfiles/.gitconfig     ~/.gitconfig
+ln -s ./dotfiles/.gitignore     ~/.gitignore
+ln -s ./dotfiles/.pypirc        ~/.pypirc
+ln -s ./dotfiles/.tmux.conf     ~/.tmux.conf
+ln -s ./dotfiles/.vim           ~/.vim
+ln -s ./dotfiles/.vimrc         ~/.vimrc
+ln -s ./dotfiles/.zsh           ~/.zsh
+ln -s ./dotfiles/.zshrc         ~/.zshrc
