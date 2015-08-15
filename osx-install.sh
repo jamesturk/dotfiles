4b2db898b31f3427acb9dacbdf1d9bc9e3cf6058
James Turk <james.p.turk@gmail.com>
2015-08-14 23:48:11 -0400

    refactor a bit

---
+++ osx-install.sh
@@ -0,0 +1,10 @@
+ln -sFvh ~/config/dotfiles/aliases       ~/.aliases
+# skip bin, config, fonts
+ln -sFvh ~/config/dotfiles/gitconfig     ~/.gitconfig
+ln -sFvh ~/config/dotfiles/gitignore     ~/.gitignore
+ln -sFvh ~/config/dotfiles/pypirc        ~/.pypirc
+ln -sFvh ~/config/dotfiles/tmux.conf     ~/.tmux.conf
+ln -sFvh ~/config/dotfiles/vim           ~/.vim
+ln -sFvh ~/config/dotfiles/vimrc         ~/.vimrc
+ln -sFvh ~/config/dotfiles/zsh           ~/.zsh
+ln -sFvh ~/config/dotfiles/zshrc         ~/.zshrc
