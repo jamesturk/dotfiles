71d4a0f2d42f1524b42180125be52e06b80bfd0a
James Turk <james.p.turk@gmail.com>
2017-04-28 00:20:55 -0400

    fish

---
+++ dotfiles/config/fish/functions/fish_prompt.fish
@@ -0,0 +1,16 @@
+function fish_prompt
+    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+      set -g VIRTUAL_ENV_DISABLE_PROMPT true
+   end
+
+   if test $VIRTUAL_ENV
+       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+   end
+
+   set_color 207050
+   printf '%s' (prompt_pwd)
+   set_color normal
+
+   set_color normal
+   printf '$ '
+end
