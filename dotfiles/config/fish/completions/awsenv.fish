3ce09d879e6670924d97840c82f2cc1f5f406eb3
James Turk <dev@jamesturk.net>
2019-03-28 19:34:41 -0400

    awsenv fix

--- dotfiles/config/fish/completions/awsenv.fish
+++ dotfiles/config/fish/completions/awsenv.fish
@@ -1,1 +1,1 @@
-complete -c awsenv -x -a (python -c "import os; f=open(os.path.expanduser('~james/.aws/credentials'));  print(' '.join(line.strip(' []\n') for line in f.readlines() if line.startswith('[')))")
+complete -c awsenv -x -a (python -c "import os; f=open(os.path.expanduser('~/.aws/credentials'));  print(' '.join(line.strip(' []\n') for line in f.readlines() if line.startswith('[')))")
