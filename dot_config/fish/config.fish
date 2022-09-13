82e25721ee1e384dc710e192717a69c6a18c8755
James Turk <dev@jamesturk.net>
2022-09-13 12:50:52 -0500

    updated fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,10 +1,14 @@
 # eval (python -m virtualfish compat_aliases)
 set -g fish_greeting ""
 
-set PATH ~/.poetry/bin/ $PATH
+#set PATH ~/.poetry/bin/ $PATH
 set PATH ~/.bin/ $PATH
 set PATH ~/.dotnet/tools $PATH
 set HOMEBREW_NO_AUTO_UPDATE 1
 
-source ~/.cargo/env
-source /usr/local/opt/asdf/asdf.fish
+#source ~/.cargo/env
+#source /usr/local/opt/asdf/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# Created by `pipx` on 2022-09-13 17:47:04
+set PATH $PATH /Users/jamesturk/.local/bin
