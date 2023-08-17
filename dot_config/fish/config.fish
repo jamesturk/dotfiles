b9dea9887e28acd45773d3d705d593235ebf439c
James Turk <dev@jamesturk.net>
2023-08-17 18:24:34 -0500

    sqlite

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,7 @@ set -g fish_greeting ""
 #set PATH ~/.poetry/bin/ $PATH
 set PATH ~/.bin/ $PATH
 set PATH ~/.dotnet/tools $PATH
+set PATH "/opt/homebrew/opt/sqlite/bin" $PATH
 set HOMEBREW_NO_AUTO_UPDATE 1
 
 #source ~/.cargo/env
@@ -12,3 +13,4 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Created by `pipx` on 2022-09-13 17:47:04
 set PATH $PATH /Users/jamesturk/.local/bin
+set PATH $PATH ~/.cargo/bin
