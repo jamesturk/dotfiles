f7579fa1397ff34799be6e21e1fa3f34de0aa82a
James Turk <dev@jamesturk.net>
2023-11-02 15:42:59 -0500

    office screen

---
+++ dot_hammerspoon/ext/executable_screen-office.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export PATH="/usr/local/bin/:$PATH"
+#displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:477C1E99-8F38-41BF-9E59-B464C8F9369A res:3840x2160 hz:24 color_depth:8 enabled:true scaling:off origin:(-3840,0) degree:0"
+
+displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1512x982 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:477C1E99-8F38-41BF-9E59-B464C8F9369A res:1920x1080 hz:24 color_depth:8 enabled:true scaling:on origin:(-1920,0) degree:0"
